fb551b1a6b44b3c78b0df453d100cb054efd4b44
Nikolay <belkaleonidov@gmail.com>
2025-03-15 16:13:08 +0300

    second commit

--- app/src/main/java/com/example/dz_mp_java/MainActivity.java
+++ app/src/main/java/com/example/dz_mp_java/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.dz_mp_java;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,16 +17,50 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate() вызван");
 
         // Переход на вторую активность программно
-        Button btnProgrammatic = findViewById(R.id.button);
-        btnProgrammatic.setOnClickListener(v -> openSecondActivity(v));
+        Button btnProgrammatic = findViewById(R.id.btn_programmatic);
+        btnProgrammatic.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putExtra("fullName", "Белицкий Николай Константинович");
+            intent.putExtra("group", "ИКБО-67-23");
+            intent.putExtra("age", 19);
+            intent.putExtra("grade", 5);
+            startActivity(intent);
+        });
     }
 
-    public void openSecondActivity(View view) {
-        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        intent.putExtra("fullName", "Белицкий Николай Константинович");
-        intent.putExtra("group", "ИКБО-67-23");
-        intent.putExtra("age", 19);
-        intent.putExtra("grade", 99);
-        startActivity(intent);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() вызван"); // Активность становится видимой пользователю
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() вызван"); // Активность получает фокус и становится активной
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() вызван"); // Активность теряет фокус, но еще видима (например, открыли другую активность)
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() вызван"); // Активность больше не видима пользователю
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() вызван"); // Активность возвращается из состояния остановки
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() вызван"); // Активность уничтожается
     }
 }
